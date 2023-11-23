dfad17ffb4f39637825fc733c19f4008e8c7897c
Max Guraevskiy <maximguraevskiy@gmail.com>
2023-11-23 17:36:22 +0300

    init proj

---
+++ app/stars/stars.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./stars.scss";
+
+function Stars() {
+  return (
+    <div className="night">
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+    </div>
+  );
+}
+
+export default Stars;
